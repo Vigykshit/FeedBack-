c4e4b3e4f68ade1fa2ffecede782a282db1e8b84
Vigykshit katoch <katoch.vigy96@gmail.com>
2024-03-13 16:00:11 +0530

    first commit

--- feedback.js
+++ feedback.js
@@ -1,12 +1,12 @@
-// script.js
 
-// To access the stars
+
+
 let stars = 
 	document.getElementsByClassName("star");
 let output = 
 	document.getElementById("output");
 
-// Funtion to update rating
+
 function gfg(n) {
 	remove();
 	for (let i = 0; i < n; i++) {
@@ -20,7 +20,7 @@ function gfg(n) {
 	output.innerText = "Rating is: " + n + "/5";
 }
 
-// To remove the pre-applied styling
+
 function remove() {
 	let i = 0;
 	while (i < 5) {
